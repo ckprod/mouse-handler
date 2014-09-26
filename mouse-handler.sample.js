5ce093542ee65c06ddc585651c27d6671454bee9
irhc <krisi_kletz@gmx.de>
2014-09-26 10:09:26 +0200

    sample bug fixed

--- mouse-handler.sample.js
+++ mouse-handler.sample.js
@@ -204,7 +204,7 @@
         // set options
 		var newOptions = {};
         for (var opt in this.options)
-			newOptions[opt] = options[opt] || this.options[opt];
+			newOptions[opt] = options[opt] === 'undefined' ?  this.options[opt] : options[opt];
 		this.options = newOptions;
 		
 		this._init();
@@ -386,7 +386,7 @@
 	
     function SampleDragClick(dragElement, dragArea, statusElement, options) {
 
-        var sampleHandler = new SampleHandler(dragElement, dragArea, statusElement, options);
+        var sampleHandler = new SampleHandler(dragElement, dragArea, statusElement, options || {});
 		
         // attach handlers
         addEvent(dragElement, 'mousedown', function (event) {
